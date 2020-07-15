d7cddb0f85224ba35aec4b3722c41e620cc65c6b
shibukazu <shibukazu@hotmail.co.jp>
2020-07-15 16:37:05 +0900

    商品モデル生成および管理者モデル、商品モデル両者へのインデックス追加

---
+++ shopping_site/db/migrate/20200715073145_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :image_url
+      t.integer :price
+
+      t.timestamps
+    end
+
+    add_index :items, :name, unique: true
+    add_index :admins, :email, unique: true
+
+  end
+end
--- shopping_site/db/schema.rb
+++ shopping_site/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_152221) do
+ActiveRecord::Schema.define(version: 2020_07_15_073145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2020_07_14_152221) do
     t.boolean "master", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_items_on_name", unique: true
   end
 
 end
