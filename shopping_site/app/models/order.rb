6c19ae1d7c14b845c6f1f2b5af13b63e7f20f6a7
shibukazu <shibukazu@hotmail.co.jp>
2020-07-16 20:16:41 +0900

    orderモデルを削除

--- shopping_site/app/models/order.rb
+++
@@ -1,6 +0,0 @@
-class Order < ApplicationRecord
-    belongs_to :user
-    has_many :carts, dependent: :destroy
-    validates :user_id, presence: true
-    validates :status, presence: true
-end
--- shopping_site/db/migrate/20200716102143_add_column_to_cart2.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToCart2 < ActiveRecord::Migration[6.0]
-  def change
-    add_column :carts, :status, :integer, default: 0
-  end
-end
