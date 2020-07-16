65e8aacb7268f50ae20a0426d719f24611b22963
shibukazu <shibukazu@hotmail.co.jp>
2020-07-16 12:27:00 +0900

    サインアップに関する変更
    ルートページへのサインアップリンクの追加
    およびサインアップ時に自動ログインするように変更

--- shopping_site/app/controllers/users_controller.rb
+++ shopping_site/app/controllers/users_controller.rb
@@ -27,6 +27,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = 'User was successfully created'
+      log_in_as_user(@user)
       redirect_to root_url
     else
       render "new"
