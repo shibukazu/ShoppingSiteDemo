2e15782e6d0f9ba2e95860fa70e564f0ba828aa4
shibukazu <shibukazu@hotmail.co.jp>
2020-07-18 11:12:55 +0900

    Orderモデルに関するテストの追加

--- shopping_site/app/models/order.rb
+++ shopping_site/app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
     has_many :order_pairs, dependent: :destroy
     has_many :items, through: :order_pairs
     belongs_to :user
+    validates :status, presence: true
+    
 end
--- shopping_site/test/models/order_test.rb
+++ shopping_site/test/models/order_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:test1)
+    @item = items(:Test1)
+    @order_pair = OrderPair.new(item_id: @item.id)
+    @order = Order.new(user_id: @user.id, status: 0)
+    
+  end
+
+
+  test "should be valid" do
+    @order.order_pairs << @order_pair
+    assert @order.valid?
+  end
+
+
+  test "don't have to have order_pair" do
+    assert @order.valid?
+  end
+
+  test "user_id should be present" do
+    @order.user_id = ""
+    assert_not @order.valid?
+  end
+
+  test "status should be present" do
+    @order.status = ""
+    assert_not @order.valid?
+  end
+
+  test "should get user info" do
+    assert @order.user == @user
+  end
+
+  test "should get order items" do
+    @order.order_pairs << @order_pair
+    @order_pair.save
+    assert @item == @order.items.first
+  end
+
+  
+  
 end
