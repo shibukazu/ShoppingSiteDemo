55ff9af9cbadad8d12a49b888082d4c363cc0de8
shibukazu <shibukazu@hotmail.co.jp>
2020-07-18 20:22:28 +0900

    検索機能の実装

--- shopping_site/app/controllers/items_controller.rb
+++ shopping_site/app/controllers/items_controller.rb
@@ -5,15 +5,17 @@ class ItemsController < ApplicationController
         # GET /admins
         # GET /admins.json
         def index
+            items = Item.search_by_name(params[:search])
+            @search_word = params[:search]
             if params[:sort].nil?
-                @items = Item.order(name: "ASC").page(params[:page]).per(20)
+                @items = items.order(name: "ASC").page(params[:page]).per(20)
             else
                 if params[:sort] == "1"
-                    @items = Item.order(created_at: "DESC").page(params[:page]).per(20)
+                    @items = items.order(created_at: "DESC").page(params[:page]).per(20)
                 elsif params[:sort] == "2"
-                    @items = Item.order(created_at: "ASC").page(params[:page]).per(20)
+                    @items = items.order(created_at: "ASC").page(params[:page]).per(20)
                 else
-                    @items = Item.order(name: "ASC").page(params[:page]).per(20)
+                    @items = items.order(name: "ASC").page(params[:page]).per(20)
                 end
             end
         end
--- shopping_site/app/models/item.rb
+++ shopping_site/app/models/item.rb
@@ -6,4 +6,12 @@ class Item < ApplicationRecord
     has_many :items, through: :carts
     has_many :order_pairs, dependent: :destroy
     has_many :orders, through: :order_pairs
+
+    def self.search_by_name(search)
+        if search.nil?
+            return Item.all
+        else 
+            return Item.where("UPPER(name) LIKE ?", "%#{search.upcase}%")
+        end
+    end
 end
