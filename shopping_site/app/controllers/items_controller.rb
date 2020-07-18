8e19471867eba25b590b978c59facec95f3dfc75
shibukazu <shibukazu@hotmail.co.jp>
2020-07-18 17:17:02 +0900

    アイテム表示ページにおけるソート機能の実装

--- shopping_site/app/controllers/items_controller.rb
+++ shopping_site/app/controllers/items_controller.rb
@@ -5,7 +5,17 @@ class ItemsController < ApplicationController
         # GET /admins
         # GET /admins.json
         def index
-            @items = Item.all.order(updated_at: "DESC").page(params[:page]).per(10)
+            if params[:sort].nil?
+                @items = Item.order(name: "ASC").page(params[:page]).per(20)
+            else
+                if params[:sort] == "1"
+                    @items = Item.order(created_at: "DESC").page(params[:page]).per(20)
+                elsif params[:sort] == "2"
+                    @items = Item.order(created_at: "ASC").page(params[:page]).per(20)
+                else
+                    @items = Item.order(name: "ASC").page(params[:page]).per(20)
+                end
+            end
         end
 
         # GET /admins/new
--- shopping_site/app/controllers/static_controller.rb
+++ shopping_site/app/controllers/static_controller.rb
@@ -4,6 +4,17 @@ class StaticController < ApplicationController
   end
 
   def home
-    @items = Item.page(params[:page]).per(20)
+    
+    if params[:sort].nil?
+      @items = Item.order(name: "ASC").page(params[:page]).per(20)
+    else
+      if params[:sort] == "1"
+        @items = Item.order(created_at: "DESC").page(params[:page]).per(20)
+      elsif params[:sort] == "2"
+        @items = Item.order(created_at: "ASC").page(params[:page]).per(20)
+      else
+        @items = Item.order(name: "ASC").page(params[:page]).per(20)
+      end
+    end
   end
 end
